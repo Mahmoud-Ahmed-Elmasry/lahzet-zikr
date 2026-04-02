3ea221389a7f912d34d1698bfc5647d4eb1463cc
Mahmoud-Ahmed-Elmasry <mahmouahmad911@gmail.com>
2026-04-02 05:09:33 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,31 @@
-// ده كود بسيط عشان المتصفح يقبل التثبيت
-self.addEventListener('fetch', function(event) {
-    // هنا ممكن مستقبلاً نخلي الموقع يشتغل أوفلاين تماماً
+const CACHE_NAME = 'lahzet-zikr-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// تثبيت ملفات الموقع في الذاكرة عشان يفتح أسرع ويشتغل أوفلاين
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+// تفعيل السيرفس وركر
+self.addEventListener('activate', evt => {
+  console.log('Service Worker activated');
+});
+
+// جلب البيانات من الكاش لو مفيش نت
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request);
+    })
+  );
 });
